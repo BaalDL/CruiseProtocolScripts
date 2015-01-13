BaalDL/CruiseProtocolScripts

commit 7d6a2ebfc6fa2c8ad2d047286c171f81a59c3f1a

indistinguishable changes; read description
move itemheader execute location
move itemhandler method from battlealgorithms to items.cps
battlehandler now depends on only enemyparty(not party)

and much... sorry!

scripts/battlealgorithms.cps
@@ -43,20 +43,18 @@
 
   party = {}
   enemyparty = {}
+  targettable = {}
 
 function initializebattle(battle)
   local p = battle.party
   local e = battle.enemyparty
   orderedCharacters = {}
   battleStopWatch = 200
-  targettable = {}
-  targettable["10"] = battle.party
+  
   targettable["100"] = battle.enemyparty
   for k, v in pairs(p) do
     p[k].turntime = math.floor((battleStopWatch / p[k].physicalSpeed) + math.random(10))
 	  table.insert(orderedCharacters, p[k]) 
-	  p[k].targetnumber = 10 + k
-	  targettable[tostring(10+k)] = p[k]
   end
   for k, v in pairs(e) do
     e[k].turntime = math.floor((battleStopWatch / e[k].physicalSpeed) + math.random(10))
@@ -83,11 +81,10 @@ function progressturntime(X)
   end
 end
 
-function battlehandler(battleparty, enemyparty)
+function battlehandler(enemyparty)
 	currentbattle = {}
-  party = battleparty
-	currentbattle.party = battleparty
-	currentbattle.enemyparty = enemyparty
+	currentbattle.party = party
+  currentbattle.enemyparty = enemyparty
 	initializebattle(currentbattle)
 	battlefirstturn(currentbattle)
 	while (not checkbattleend()) do
@@ -180,6 +177,7 @@ function askplayer(char, party, enemyparty, battle)
   local pc = 0
   local itemmove
   while (not playercommand) do
+    local targets
     itemmove = -2
     moveto(startaskt, startaskl)
 	  if char.attackType then
@@ -201,29 +199,25 @@ function askplayer(char, party, enemyparty, battle)
     if (pc <= 8 and pc >= 0) then
 	    playertarget = lookuptarget(char.skills[pc].MoveType, char.skills[pc].Target, char.targetnumber)
 	    if (playertarget == "100") then
-        local targets = enemyparty
-        skillhandler(char, char.skills[pc], targets)
+        targets = enemyparty
       elseif (playertarget == "10") then
-        local targets = party
-        skillhandler(char, char.skills[pc], targets)
+        targets = party
       elseif (playertarget ~= "-1") then
-        local targets = {targettable[playertarget]}
-	      skillhandler(char, char.skills[pc], targets)
+        targets = {targettable[playertarget]}
       end
+      skillhandler(char, char.skills[pc], targets)
     elseif (pc == 10) then
       itemmove = inventorymenu("battle")
       if (itemmove ~= -1) then
-        playertarget = lookuptarget(ItemList[itemmove].ItemType, ItemList[itemmove].Target, char.targetnumber)
+        playertarget = itemtarget(ItemList[itemmove].ItemType, ItemList[itemmove].Target, char.targetnumber)
         if (playertarget == "100") then
-          local targets = enemyparty
-          itemhandler(char, itemmove, targets)
+          targets = enemyparty
         elseif (playertarget == "10") then
-          local targets = party
-          itemhandler(char, itemmove, targets)
+          targets = party
         elseif (playertarget ~= "-1") then
-          local targets = {targettable[playertarget]}
-          itemhandler(char, itemmove, targets)
+          targets = {targettable[playertarget]}
         end
+        itemhandler(char, itemmove, targets)
       end
 	  end
     if (playertarget == "-1") or (itemmove == -1) then
@@ -234,7 +228,9 @@ function askplayer(char, party, enemyparty, battle)
       erase(startaskt, endaskt)
     end
   end
-  if (pc <= 8 and pc >= 0) then
+  if (pc == -1) then
+    return nil
+  elseif (pc <= 8 and pc >= 0) then
     return char.skills[pc]
   elseif pc == 10 then
     return SkillList["UseItem"]
@@ -352,20 +348,6 @@ function skillhandler(char, skill, skilltarget)
     end
 	end
 
-end
-
-function itemhandler(char, item, itemtarget)
-  local itemdata = ItemList[item]
-  message = message .. char.name .. "(은)는 " .. itemdata.Name .. "(을)를 사용했다!"
-  if (itemdata.ItemType == "Heal") then
-    for k, v in pairs(itemtarget) do
-      applyheal(char, itemdata, itemtarget[k])
-    end
-  end
-  inventory[item] = inventory[item] - 1
-  if (inventory[item] == 0) then
-    message = message .. "\n가지고 있던 " .. itemdata.Name .. "(을)를 모두 사용해 버렸다!"
-  end
 end
 
 function calculatedamage(actor, skill, target, battle)

scripts/debug_implement.cps
@@ -63,11 +63,14 @@
 
   execute ("skilllist")
   execute ("enemylist")
+  execute ("itemlist")
+  execute ("partyheader")
   execute ("partydata")
   execute ("enemypartylist")
   execute ("enemypartyheader")
   execute ("battleheader")
-
+  execute ("itemheader")
+  
   addsave("allpartymembers")
   addsave("currentpartymembers")
   addsave("EnemyInfo")

scripts/items.cps
@@ -1,59 +1,98 @@
 #function itemheader
-  execute("itemlist")
 
   function useitem(touse)
     local itemdata = ItemList[touse]
+    local targets
+    printl(itemdata.Name .. "(을)를 사용했다!")
     if itemdata.ItemType == "Heal" then
-      local target = lookuptarget("Heal", party)
+      local target = itemtarget("Heal", "AnAlly")
+      if (target == "100") then
+        targets = enemyparty
+      elseif (target == "10") then
+        targets = party
+      elseif (target ~= "-1") then
+        targets = {targettable[target]}
+      end
+      itemhandler(char, touse, targets)
     end
+    printl(message)
+    message = ""
   end
 
   function itemtarget(type, target)
-  local targets = {"-1"}
-  local p = currentbattle.party
-  local e = currentbattle.enemyparty
-  local selection
+    local targets = {"-1"}
+    local p = partyhandler(currentpartymembers)
+    local e
+    if currentbattle then
+      p = currentbattle.party
+      e = currentbattle.enemyparty
+    end
+    local selection
 
-  if target == "AnEnemy" then
-    for k, v in pairs(e) do
-      if e[k].alive then table.insert(targets, tostring(e[k].targetnumber)) end
+    if target == "AnEnemy" then
+      for k, v in pairs(e) do
+        if e[k].alive then table.insert(targets, tostring(e[k].targetnumber)) end
+      end
+      table.sort(targets)
+      if type == "Attack" then
+        selection = ask("누구를 공격합니까?", unpack(targets))
+      elseif type == "Harass" then
+        selection = ask("누구를 목표로 삼습니까?", unpack(targets))
+      end
+    elseif target == "WholeEnemy" or
+      target == "RandomEnemies" or
+      target == "PWREnemies" then
+      selection = ask("적 전체를 목표로 삼습니까?", "-1", "100")
+    elseif target == "AnAlly" then
+      for k, v in pairs(p) do
+        if p[k].alive then table.insert(targets, tostring(p[k].targetnumber)) end
+      end
+      table.sort(targets)
+      if type == "Heal" or
+        type == "Support" then
+        selection = ask("누구를 목표로 삼습니까?", unpack(targets))
+      end
+    elseif target == "AnAllyIncludingDead" then
+      for k, v in pairs(p) do
+        table.insert(targets, tostring(p[k].targetnumber))
+      end
+      table.sort(targets)
+      if type == "Heal" or
+        type == "Support" then
+        selection = ask("누구를 목표로 삼습니까?", unpack(targets))
+      end
+    elseif target == "WholeAlly" then
+      selection = ask("아군 전체를 목표로 삼습니까?", "-1", "10")
+    elseif target == "Self" then
+      selection = ask("자기 자신을 목표로 삼습니까?", "-1", selfnum.toString)
     end
-    table.sort(targets)
-    if type == "Attack" then
-      selection = ask("누구를 공격합니까?", unpack(targets))
-    elseif type == "Harass" then
-      selection = ask("누구를 목표로 삼습니까?", unpack(targets))
-    end
-  elseif target == "WholeEnemy" or
-    target == "RandomEnemies" or
-    target == "PWREnemies" then
-    selection = ask("적 전체를 목표로 삼습니까?", "-1", "100")
-  elseif target == "AnAlly" then
-    for k, v in pairs(p) do
-      if p[k].alive then table.insert(targets, tostring(e[k].targetnumber)) end
-    end
-    table.sort(targets)
-    if type == "Heal" or
-      type == "Support" then
-      selection = ask("누구를 목표로 삼습니까?", unpack(targets))
-    end
-  elseif target == "AnAllyIncludingDead" then
-    for k, v in pairs(p) do
-      table.insert(targets, tostring(e[k].targetnumber))
-    end
-    table.sort(targets)
-    if type == "Heal" or
-      type == "Support" then
-      selection = ask("누구를 목표로 삼습니까?", unpack(targets))
-    end
-  elseif target == "WholeAlly" then
-    selection = ask("아군 전체를 목표로 삼습니까?", "-1", "10")
-  elseif target == "Self" then
-    selection = ask("자기 자신을 목표로 삼습니까?", "-1", selfnum.toString)
+
+    return selection
   end
 
-  return selection
-end
+  function itemhandler(char, item, itemtarget)
+    if not item then
+      printl ("==NEED DEBUG== no item!")
+      return
+    end
+    local itemdata = ItemList[item]
+    if not message then message = "" end
+    if char then
+      message = message .. char.name .. "(은)는 " .. itemdata.Name .. "(을)를 사용했다!"
+    else
+      message = message .. itemdata.Name .. "(을)를 사용했다!"
+    end
+    if (itemdata.ItemType == "Heal") then
+      for k, v in pairs(itemtarget) do
+        applyheal(char, itemdata, itemtarget[k])
+      end
+    end
+    inventory[item] = inventory[item] - 1
+    if (inventory[item] == 0) then
+      message = message .. "\n가지고 있던 " .. itemdata.Name .. "(을)를 모두 사용해 버렸다!"
+    end
+  end
+
 #end
 
 #function itemlist

scripts/startgame.cps
@@ -7,7 +7,6 @@
   execute ("loadptypebattlefunctions")
   execute ("configurebasicparameters")
   execute ("loadbattledata")
-  execute ("itemheader")
   execute ("dungeonsearchfunctions")
   execute ("playmenu")
   activatesubmenu(1,2,3,4,5,6,7,8,9)
@@ -18,13 +17,14 @@
   inventory[103] = 5
   inventory[104] = 5
   table.sort(inventory)
+  party = partyhandler(currentpartymembers)
   local playerchoice
   while(playerchoice ~= "-1") do
     printl ("[1] 전투 [2] 저장 [3] 탐험 [4] 로드 [5] 메뉴 [-1] 종료")
     playerchoice = ask("무엇을 합니까?", "1", "2", "3", "4", "5", "-1")
     if playerchoice == "1" then
       enemyparty = initializeenemyparty(EnemyPartyTempletes["thugs1"])
-      battlehandler(currentpartymembers, enemyparty)
+      battlehandler(enemyparty)
     elseif playerchoice == "2" then
       save("savetemp.txt")
     elseif playerchoice == "3" then

commit 7618d7b7a2f16ff26e19ff8ac097b8719e7dad72

error fix
stupid error caused by 7d6a2ebfc6fa2c8ad2d047286c171f81a59c3f1a

scripts/battlealgorithms.cps
@@ -211,26 +211,31 @@
     
       if (pc <= 8 and pc >= 0) then
   	    playertarget = lookuptarget(char.skills[pc].MoveType, char.skills[pc].Target, char.targetnumber)
-  	    if (playertarget == "100") then
-          targets = enemyparty
-        elseif (playertarget == "10") then
-          targets = party
-        elseif (playertarget ~= "-1") then
-          targets = {targettable[playertarget]}
-        end
-        skillhandler(char, char.skills[pc], targets)
-      elseif (pc == 10) then
-        itemmove = inventorymenu("battle")
-        if (itemmove ~= -1) then
-          playertarget = itemtarget(ItemList[itemmove].ItemType, ItemList[itemmove].Target, char.targetnumber)
+  	    
+        if (playertarget ~= "-1") then
           if (playertarget == "100") then
             targets = enemyparty
           elseif (playertarget == "10") then
             targets = party
-          elseif (playertarget ~= "-1") then
+          else
             targets = {targettable[playertarget]}
           end
-          itemhandler(char, itemmove, targets)
+          skillhandler(char, char.skills[pc], targets)
+        end
+      elseif (pc == 10) then
+        itemmove = inventorymenu("battle")
+        if (itemmove ~= -1) then
+          playertarget = itemtarget(ItemList[itemmove].ItemType, ItemList[itemmove].Target, char.targetnumber)
+          if (playertarget ~= "-1") then
+            if (playertarget == "100") then
+              targets = enemyparty
+            elseif (playertarget == "10") then
+              targets = party
+            else
+              targets = {targettable[playertarget]}
+            end
+            itemhandler(char, itemmove, targets)
+          end
         end
       elseif (pc == 99) then
         fleemove = tonumber(confirmflee())